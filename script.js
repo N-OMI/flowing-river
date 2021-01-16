7e5f950a59eac7ce0eeb8bad0714a2dc8e1de945
Naomi <naomiejarrell@gmail.com>
2021-01-15 23:11:30 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+// Yamato yoshinoyama. [Translation: Yoshino Mountain in Yamato Province]. Woodcut print by Utagawa Hiroshige, 1859. Library of Congress Prints & Photographs Division. https://www.loc.gov/resource/jpd.01362/
